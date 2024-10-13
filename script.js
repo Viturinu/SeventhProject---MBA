47c8423c187af0fbc6cc03ccb5e8fcb40fe08043
Victor Oliveira Almeida <victor.almeida.ti.celular@gmail.com>
2024-10-13 12:38:26 -0300

    convertCurrency

---
+++ script.js
@@ -0,0 +1,36 @@
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
+
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+const form = document.querySelector("form");
+
+amount.addEventListener("input", () => {
+
+    const hasCharactersRegex = /\D+/g;
+    amount.value = amount.value.replace(hasCharactersRegex, "");
+})
+
+form.onsubmit = function (event) {
+    event.preventDefault();
+
+    switch (currency.value) {
+        case "USD":
+            convertCurrency(amount.value, USD, "US$");
+            break;
+
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€");
+            break;
+
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£");
+            break;
+    }
+
+}
+
+function convertCurrency(amount, price, symbol) {
+    console.log(amount, price, symbol);
+}
