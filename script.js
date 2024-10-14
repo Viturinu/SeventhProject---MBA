a375164f93883835544b96108adcf08578f8d135
Victor Oliveira Almeida <victor.almeida.ti@gmail.com>
2024-10-14 16:34:50 -0300

    Pronto

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const GBP = 6.08;
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const form = document.querySelector("form");
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+const result = document.querySelector("h1#result");
 
 amount.addEventListener("input", () => {
 
@@ -32,5 +35,28 @@ form.onsubmit = function (event) {
 }
 
 function convertCurrency(amount, price, symbol) {
-    console.log(amount, price, symbol);
+    // console.log(amount, price, symbol);
+    try {
+        footer.classList.add("show-result");
+
+        description.textContent = `${symbol} 1 =  ${formatCurrencyBRL(price)}`;
+        let total = price * amount;
+        
+        if(isNaN(total)){
+            return alert("Por favor, digite o valor corretamente para converter.")
+        }
+
+        total = formatCurrencyBRL(price * amount).replace("R$", "");
+        result.textContent = `${total} Reais`;
+    } catch (error) {
+        console.log("Não foi possível converter. Tente novamente mais tarde. Erro = ", error);
+        footer.classList.remove("show-result")
+    }
+}
+
+function formatCurrencyBRL(value){
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
 }
